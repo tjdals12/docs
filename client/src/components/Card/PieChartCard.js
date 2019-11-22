84b6eeedaffc37b20ec1a92c4c0484bf16d05585
Seongmin <lll2slll@naver.com>
2019-11-22 17:53:31 +0900

    [2019. 11. 22] 1. Dashboard > Vendor 관련 차트 추가, API 연동 전

---
+++ client/src/components/Card/PieChartCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Card, CardHeader, CardBody } from 'reactstrap';
+import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+
+const PieChartCard = ({ data, className }) => {
+    const classes = classNames(className, 'w-100', 'h-100');
+    const RADIAN = Math.PI / 180;
+
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, name }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 1.2;
+        const x = cx + radius * Math.cos(midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text className="title-font" x={x} y={y} textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${name} ${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    }
+
+    return (
+        <Card className={classes}>
+            <CardHeader className="bg-white title-font">
+                업체 비율 (%)
+            </CardHeader>
+            <CardBody>
+                <ResponsiveContainer minHeight={400}>
+                    <PieChart>
+                        <Pie
+                            data={data} 
+                            dataKey="value"
+                            label={renderCustomizedLabel}
+                        >
+                            {data.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)}
+                        </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default PieChartCard;
